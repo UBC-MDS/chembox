c6a540afc28712cbe35cb2a81e43ba2a4447cae1
Luke Yang <lukey.ubc@qq.com>
2023-01-12 10:25:02 -0800

    add function documentation to is_valid

--- src/chembox/chembox.py
+++ src/chembox/chembox.py
@@ -4,8 +4,30 @@ def get_elements():
     # for Nate
     return pd.DataFrame()
 
-def is_valid():
-    # for Luke
+def is_valid(formula: str) -> bool: 
+    """
+    Check if a given string chemical formula is chemically valid
+
+    Parameters
+    ----------
+    formula : str
+        Input chemical formula
+
+    Returns
+    -------
+    is_valid : bool
+        True if the chemical formula is valid and false otherwise
+
+    Examples
+    --------
+    >>> is_valid('CH')
+    False
+    >>> is_valid('CH4')
+    True
+    >>> is_valid('CH3COOH')
+    True
+    """
+    
     return True
 
 def get_molar_mass():
