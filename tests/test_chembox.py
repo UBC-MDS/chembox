3a23a5363f0de3d27bf851b9ce721aa29288e831
Luke Yang <lukey.ubc@qq.com>
2023-01-18 00:09:27 -0800

    add tests

--- tests/test_chembox.py
+++ tests/test_chembox.py
@@ -1,1 +1,30 @@
-from chembox import chembox
+from chembox.chembox import *
+
+import pytest
+
+def test_is_valid():
+
+    # Test for Value
+    with pytest.raises(ValueError):
+        # check if ValueError is raised when the element input is wrong
+        is_valid('Nx2CO3')
+
+    with pytest.raises(ValueError):
+        # check if ValueError is raised when the oxidation state is not unique
+        # here, carbon alone can have multiple oxidation states
+        is_valid('C2H6')
+
+    # Test for calcium carbonate (a common salt)
+    assert is_valid('Al2(SO4)3')
+
+    # Test for sodium hydroxide (a common base)
+    assert is_valid('NaOH')
+
+    # Test for chloric acid (a common acid)
+    assert is_valid('HCl')
+
+    # Test for invalid chemical (too many chlorines)
+    assert not is_valid('HCl10')
+
+     # Test for invalid chemical (too few sodiums)
+    assert is_valid('NaCO3')
